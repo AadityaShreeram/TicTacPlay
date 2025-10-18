ed20b8d6452741b3ae4374392666109a423c4f70
Aaditya S <aaditya.r@zuper.co>
2025-10-19 02:04:43 +0530

    Updated server port

--- backend/src/index.js
+++ backend/src/index.js
@@ -608,6 +608,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
   console.log(`TicTacPlay backend running on port ${PORT}`);
 });
